cc977497edd428c381dd7945b1192224efeeccb3
leo_pep_ <pepinus.leo@gmail.com>
2025-07-28 21:37:00 +0200

    Add initial project files and configuration for Flutter app

---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_dart_app/main.dart';
+
+void main() {
+ // todo: write complete test suite
+}
